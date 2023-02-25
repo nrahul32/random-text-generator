d469b2895aac0cdd90a8d1c963f734958d503d8d
Rahul N <rahul.eee32@gmail.com>
2023-02-25 14:36:18 +0530

    Using state to store format and para count

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     super(props);
 
     this.state = {
-      text: ''
+      paras: 5,
+      format: "html",
+      text: ""
     }
   }
 
@@ -17,7 +19,7 @@ class App extends Component {
   }
 
   getSampleText() {
-    axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=5&format=text')
+    axios.get("https://baconipsum.com/api/?type=meat-and-filler&paras=" + this.state.paras + "&format=" + this.state.format)
       .then(resp => {
         this.setState({ text: resp.data }, function () {
           console.log(this.state);
@@ -30,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Random Text Generator
+        <h1>Random Text Generator</h1>
         <Output value={this.state.text} />
       </div>
     );
