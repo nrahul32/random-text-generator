3951cb84977523b5c816ed9f8ce7e4b7c2fb92bf
Rahul N <rahul.eee32@gmail.com>
2023-02-25 13:09:15 +0530

    Using axios to get random text and storing in state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import axios from 'axios';
 import { Component } from 'react';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=5&format=text')
+      .then(resp => {
+        this.setState({ text: resp.data }, function () {
+          console.log(this.state);
+        })
+      }).catch((err) => {
+        console.log(err);
+      })
   }
 
   render() {
